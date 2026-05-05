78877479589cb408838814d0ada5c56ee3d69a4a
Sushanth Vaddi <sushanthweb@gmail.com>
2026-05-05 09:58:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://visitor-management-system-dobx.vercel.app',
+    'https://assignment-9-tutedude.vercel.app',
   ],
   credentials: true,
 }));
